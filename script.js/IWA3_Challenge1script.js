dade2303e0aee80be4b97ee28aef8237d3da20c1
Riaan Beukes <rgbsik1@gmail.com>
2023-06-28 01:23:23 +0200

    Added IWA3_Challenge1_Config.js file

---
+++ script.js/IWA3_Challenge1script.js
@@ -0,0 +1,18 @@
+// scripts.js
+
+//console.log(hello world);
+
+import company form ''
+import year form 'configuration'
+
+const message = '© ' + company + ' (' + year + ')'
+document.querySelector('footer').innerText = message
+
+
+
+// configuration.js
+
+//const company = 'ACME Inc.'
+//const year = 2022
+
+//
